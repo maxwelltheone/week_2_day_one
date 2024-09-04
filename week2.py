ac5d7f1fb4010f370b59cf1c82297cc2ef5c59ed
Luevanos <pluevanos@cps.k12.il.us>
2024-09-04 09:35:16 -0500

    something

--- week2.py
+++ week2.py
@@ -6,3 +6,4 @@ holiday = input("What's your favorite holiday?")
 print("My favorite holiday is", holiday)
 # print out the day and their favorite holiday in a sentence
 print("Today is ", day, " and my favorite holiday is", holiday)
+#making a change to the file
